8d6f34746646b781d32342cd4a187018355a880a
Vadim <24vadhad@gmail.com>
2023-12-04 10:42:31 +0100

    rework sql

--- src/config/config.php
+++ src/config/config.php
@@ -4,3 +4,8 @@ define("SRC", '../src/');
 define("CONTROLLERS", SRC . 'Controllers/');
 define("MODELS", SRC . 'Models/');
 define("VIEWS", SRC . 'Views/');
+
+define('HOST', 'localhost:8889');
+define('DATABASE', 'purplestream');
+define('USER', 'root');
+define('PASSWORD', 'root');
