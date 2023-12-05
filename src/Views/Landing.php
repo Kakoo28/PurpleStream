955e1519baddb2e6621e3dd390608b650f515c13
Kakoo28 <kfoubert@edenschool.fr>
2023-12-05 14:58:17 +0100

    home->landing

---
+++ src/Views/Landing.php
@@ -0,0 +1,16 @@
+<?php include_once ("include/Header.php"); ?>
+<link rel="stylesheet" href="/css/pages/landing.css">
+
+<main class="main main--f-end main--background">
+    <div class="container header-container">
+        <img class='logo' src="img/logo.png" alt="logo PurpleStream">
+        <h2 class="header__title">Tout vos Anime, Gratuitement !</h2>
+    </div>
+    <div class="container button-container">
+        <a href="#" class="button button--solid">👑 Voir les animes disponibles</a>
+        <a href="/login" class="button">Se connecter</a>
+        <a class="color-w" href="/register">ou <span>Créer un compte</span></a>
+    </div>
+</main>
+
+<?php include_once ("include/Footer.php"); ?>
--- src/Views/Login.php
+++ src/Views/Login.php
@@ -1,17 +1,18 @@
 <?php include_once ("include/Header.php"); ?>
+<link rel="stylesheet" href="/css/pages/user-form.css">
 
-<main id="background">
-    <div class="header-content">
+<main class="main main--f-end main--background">
+    <div class="container header-container">
         <img class='logo' src="img/logo.png" alt="logo PurpleStream">
-        <h1>Connectez-vous à votre compte</h1>
+        <h2 class="header__title">Connectez-vous à votre compte</h2>
     </div>
-    <div class="button-container">
-        <form action="/process-login" method="post">
-            <input type="text" id="username" name="username" placeholder="Email" required>
+    <div class="container form__container">
+        <form action="/process-login" method="post" class="form login-form">
+            <input type="email" id="username" name="username" placeholder="Email" required>
             <input type="password" id="password" name="password" placeholder="Mot de passe" required>
-            <button type="submit" id="btnLogin">Se connecter</button>
+            <button type="submit" class="button submit-button">Se connecter</button>
         </form>
-        <p>Vous n'avez pas de compte? <a href="/">Créer un compte</a></p>
+        <a href="/">Vous n'avez pas de compte? <span>Créer un compte</span></a>
     </div>
 </main>
 
