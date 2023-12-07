388ce88f6a103a0414956da4e9dfb4b0833ae590
Kakoo28 <kfoubert@edenschool.fr>
2023-12-07 15:42:53 +0100

    rename get/set userModel

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -24,52 +24,52 @@ class UserModel
         $this->user_role = $user_role;
     }
 
-    public function setuser_id($user_id)
+    public function setUserId($user_id)
     {
         $this->user_id = $user_id;
     }
 
-    public function getuser_id()
+    public function getUserId()
     {
         return $this->user_id;
     }
 
-    public function setuser_email($user_email)
+    public function setUserEmail($user_email)
     {
         $this->user_email = $user_email;
     }
 
-    public function getuser_email()
+    public function getUserEmail()
     {
         return $this->user_email;
     }
 
-    public function setuser_name($user_name)
+    public function setUserName($user_name)
     {
         $this->user_name = $user_name;
     }
 
-    public function getuser_name()
+    public function getUserName()
     {
         return $this->user_name;
     }
 
-    public function setuser_password($user_password)
+    public function setUserPassword($user_password)
     {
         $this->user_password = $user_password;
     }
 
-    public function getuser_password()
+    public function getUserPassword()
     {
         return $this->user_password;
     }
 
-    public function setuser_role($user_role)
+    public function setUserRole($user_role)
     {
         $this->user_role = $user_role;
     }
 
-    public function getuser_role()
+    public function getUserRole()
     {
         return $this->user_role;
     }
